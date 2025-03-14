189ce5a160b635aa201a4b81bdf3dfe5e89d38b9
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-14 13:09:29 +0530

    create model of feedback which defines schema in Database.

---
+++ back-end/models/feedback.js
@@ -0,0 +1,25 @@
+
+const mongoose = require ('mongoose');
+
+const feedbackSchema = new mongoose.Schema ({
+    studentName: String, 
+    rollNumber: Number,
+
+    subjects: { 
+        subject1: Number,
+        subject2: Number,
+        subject3: Number,
+        subject4: Number,
+        subject5: Number
+    },
+
+    practicals: {
+        practical1: Number,
+        practical2: Number,
+        practical3: Number,
+    },
+
+    feedbackComments: String
+});
+
+module.exports = mongoose.model('feedback', feedbackSchema);
